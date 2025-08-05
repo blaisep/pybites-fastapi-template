076a631a89774f785552b3360a3a1a5cde946870
Blaise Pabon <blaise@gmail.com>
2025-08-05 16:05:33 +0000

    Complete the Christmas Tree Generator bite
    Co-authored-by: Mark Nyberg <bigusdeveloper@gmail.com>
    Co-authored-by: Anschel Burk <anschel.burk@gmail.com>
    Co-authored-by: Jeff Jacobson <jeffjacobsonhimself@gmail.com>
    Co-authored-by: Bob Belderbos <bbelderbos@gmail.com>
    Co-authored-by: Gijs Mos <gmos@users.noreply.github.com>
    Co-authored-by: Kishan Patel <kishanpatel789@gmail.com>

--- main.py
+++ main.py
@@ -1,6 +1,15 @@
-def main():
-    print("Hello from pybites-fastapi-template!")
+def generate_xmas_tree(rows=10):
+    """Generate a xmas tree of stars (*) for given rows (default 10).
+       Each row has row_number*2-1 stars, simple example: for rows=3 the
+       output would be like this (ignore docstring's indentation):
+         *
+        ***
+       *****"""
+    star = "*"
+    max_width = rows*2 - 1
+    star_list = []
+    for i in range(1, rows + 1):
+        num_stars = i * 2 - 1
+        star_list.append(f"{num_stars * star:^{max_width}}")
+    return "\n".join(star_list)
 
-
-if __name__ == "__main__":
-    main()
